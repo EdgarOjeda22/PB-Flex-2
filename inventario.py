a273aaeb3dd2ff06bc3d90052016f2a1075c0082
EdgarOjeda22 <edgar.ojeda@inacapmail.cl>
2026-08-25 12:04:16 -0400

    Update inventario.py

--- inventario.py
+++ inventario.py
@@ -1,10 +1,10 @@
 class Inventario:
+
     def __init__(self):
         self.objetos = []
 
+    def agregar_objetos(self, objeto ):
 
-
-    def agregar_objetos(self,objeto):
         self.objetos.append(objeto)
 
         print(f"{objeto.nombre} ha sido agregado al inventario")
@@ -12,10 +12,11 @@ class Inventario:
     def mostrar_inventario(self):
 
         print("\n ---Inventario---")
-
-        if len(self.objetos)  == 0:
+        # validamos si nuestra lista tiene objetos en el inventario
+        if len(self.objetos) == 0:
             print("El inventario está vacio")
 
         else:
+            # recorremos el arreglo de objetos
             for objeto in self.objetos:
-                print(f"- {objeto.nombre} {(objeto.tipo)}")
+                print(f"- {objeto.nombre} ({objeto.tipo})")
