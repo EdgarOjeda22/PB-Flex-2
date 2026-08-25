c10d90263a831525e5c1ea5c26c0ac537def501c
EdgarOjeda22 <edgar.ojeda@inacapmail.cl>
2026-08-25 12:03:23 -0400

    Update guerrero.py

--- guerrero.py
+++ guerrero.py
@@ -1,19 +1,16 @@
-from personaje import Personaje 
-
+from personaje import Personaje
 
 class Guerrero(Personaje):
-   def __init__(self, nombre, nivel, vida,fuerza):
-      super().__init__(nombre,nivel,vida)
-      self.fuerza = fuerza
 
-
-
-   def atacar(self):
-      print(f"{self.nombre} realiza un ataque cuerpo a cuerpo con {self.fuerza} de fueza")
-
-
-   def usar_habilidad(self):
-      print(f"{self.nombre} utiliza un ataque especuia con su espada")
-      
-
+    def __init__(self, nombre, nivel, vida, fuerza):
+        super().__init__(nombre, nivel, vida)
+        self.fuerza = fuerza
+    
+        #Polimorfismo
+    def atacar(self):
+        print(f"{self.nombre} ataca con sus dos espadas"
+              f" con {self.fuerza} de fuerza")
+        
+    def usar_habilidad(self):
+         print(f"{self.nombre} utiliza Berserk")
    
