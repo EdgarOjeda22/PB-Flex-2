fe1b2144e561f43cd5d7eac7b2e41c6758f0948b
EdgarOjeda22 <edgar.ojeda@inacapmail.cl>
2026-08-25 12:04:44 -0400

    Update jugador.py

--- jugador.py
+++ jugador.py
@@ -1,26 +1,22 @@
-
 class Jugador:
-
-    def __init__(self,nombre):
+    
+    def __init__(self, nombre):
 
         self.nombre = nombre
         self.personaje = None
 
-
-    def selecccionar_personaje(self, personaje):
+    def seleccionar_personaje(self, personaje):
 
         self.personaje = personaje
 
-        print(f"{self.nombre} seleccionó al pj"f"  {personaje.nombre}")
-    
+        print(f"{self.nombre} seleccionó al pj"  
+              f" {personaje.nombre}") 
 
     def mostrar_personaje(self):
 
         if self.personaje is not None:
-            print(f"El jugador {self.nombre} "f"utliza a {self.personaje.nombre}")
+            print(f"El jugador {self.nombre}"
+                  f" utiliza a {self.personaje.nombre}")
 
-        else:
-            print("El jugador no a seleccionado a ningun personaje")
-
-                  
-
+        else: 
+            print("El jugador no tiene un personaje seleccionado.") 
